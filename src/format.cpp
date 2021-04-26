61d5f3c78269350e21bfe4124f30951abc455e8d
Joe Blower <jblower13@gmail.com>
2021-04-26 07:59:47 +0100

    Elapsed time helper formatting function implemented to show uptime

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,17 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+    // calculate hrs, mins, secs
+    long hours = seconds / 60 / 60;
+    int minutes = ( seconds - ( hours * 60 * 60 ) ) / 60;
+    int secs = ( seconds - ( hours * 60 * 60) - ( minutes * 60 ) );
+
+    //handle leading 0 if required
+    string hoursS = hours < 10 ? "0" + std::to_string(hours) : std::to_string(hours);
+    string minutesS = minutes < 10 ? "0" + std::to_string(minutes) : std::to_string(minutes);
+    string secsS = secs < 10 ? "0" + std::to_string(secs) : std::to_string(secs);
+
+    return hoursS + ":" + minutesS + ":" + secsS;
+    
+}

e6eba8e10941029a8294200742fff72113db2fc2
Joe Blower <jblower13@gmail.com>
2021-04-28 07:06:34 +0100

    Process uptime added (not sure if clock time should tick)

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -168,5 +168,18 @@ string LinuxParser::Uid(int pid[[maybe_unused]]) { return string(); }
 string LinuxParser::User(int pid[[maybe_unused]]) { return string(); }
 
 // TODO: Read and return the uptime of a process
-// REMOVE: [[maybe_unused]] once you define the function
-// long LinuxParser::UpTime(int pid[[maybe_unused]]) { return 0; }
+long LinuxParser::UpTime(int pid) {
+  string line, value;
+  std::ifstream stream(kProcDirectory + to_string(pid) + kStatFilename);
+  if(stream.is_open()){
+    std::getline(stream, line);
+    std::stringstream linestream(line);
+    for(int i{1}; i<=22; i++){
+      linestream >> value;
+      if(i == 22){
+        return  std::stol(value) / sysconf(_SC_CLK_TCK);
+      }
+    }
+  }
+  return 0;
+ }
